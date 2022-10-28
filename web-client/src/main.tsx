d741359a3c83a4ec760223dd3c795e5fb18fee49
Mogakamo <mogaka.amo254@gmail.com>
2022-10-28 11:26:37 +0300

    [router]: creating routes for pages

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
