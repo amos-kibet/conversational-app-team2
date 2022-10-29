d441afad32b26fab6fe8bb98a0609c1ef7b370de
Mogakamo <mogaka.amo254@gmail.com>
2022-10-29 19:15:23 +0300

    [lint]: configured lint in the project

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -1,18 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/index.css";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
-import Home from "./routes/Home";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
+	{
+		path: '/',
+		element: <Home />,
+	},
+	{
+		path: '/about',
+		element: <About />,
+	},
 ]);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
--- web-client/src/routes/Home.tsx
+++ web-client/src/routes/Home.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
-  return (
-    <div className="text-yellow-400">Home</div>
-  )
-}
+	return <div className="text-yellow-400">Home</div>;
+};
 
-export default Home
+export default Home;
