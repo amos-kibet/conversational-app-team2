8c1725b8de9393cb0f1cedfdb6d1c3c5c1d305d2
Mogakamo <mogaka.amo254@gmail.com>
2022-10-30 07:54:54 +0300

    [fix]: lint issues

--- web-client/.eslintrc.cjs
+++ web-client/.eslintrc.cjs
@@ -1,28 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "plugin:react/recommended",
-        "standard-with-typescript"
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "standard-with-typescript"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: ["tailwind"],
+      },
     ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        'at-rule-no-unknown': null,
-        'scss/at-rule-no-unknown': [
-            true,
-            {
-                'ignoreAtRules': ['tailwind']
-            }
-        ],
-    }
-}
+  },
+};
