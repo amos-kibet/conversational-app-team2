e675269594499e96b6abb120623362742a799951
amos-kibet <kibetamos97@gmail.com>
2022-12-08 11:32:27 +0300

    Update courses model

--- Backend/models/Course.js
+++ Backend/models/Course.js
@@ -1,50 +1,90 @@
 const { Schema, model } = require("mongoose");
 const Paginator = require("mongoose-paginate-v2");
 
-const CourseSchema = new Schema({
-  courseImage: {
-    type: String,
-    required: false 
+const CourseSchema = new Schema(
+  {
+    usr_id: {
+      type: Schema.Types.ObjectId,
+      ref: "users",
+    },
+    token: {
+      type: String,
+      required: false,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+    registrationNumber: {
+      type: String,
+      required: true,
+    },
+    instructors: {
+      type: Array,
+      required: true,
+    },
+    unit: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    discount: {
+      type: String,
+      required: true,
+    },
+    file: {
+      type: String,
+      required: true,
+    },
+    fileName: {
+      type: String,
+      required: true,
+    },
+    page: {
+      type: Number,
+      required: false,
+    },
+    search: {
+      type: String,
+      required: false,
+    },
+    courses: {
+      type: Array,
+      required: true,
+    },
+    pages: {
+      type: Number,
+      required: false,
+    },
+    loading: {
+      type: Boolean,
+      required: false,
+    },
   },
-  title: {
-    type: String,
-    required: true 
-  },
-  content: {
-    type: String,
-    required: true 
-  },
-  slug: {
-    type: String,
-    required: true, 
-  },
-  likes: {
-    type: Number,
-    default: 0
-  },
-  comments: [
-    {
-      text: {
-        type: String,
-        required: true 
-      },
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: "users"
-      },
-
-    }
-  ],
-  category: {
-    type: String,
-    required: false
-  },
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "users"
-  },
-},
-{ timestamps: true }
+  { timestamps: true }
 );
 
 CourseSchema.plugin(Paginator);
