63b52f9731f34f88bdc3e37c55da933d2c703c24
kanayofirms <kanyeze@gmail.com>
2022-12-10 17:42:42 +0100

    feat: edit course validator function

--- Backend/services/repo/course-validator.js
+++ Backend/services/repo/course-validator.js
@@ -1,8 +1,9 @@
 const { check } = require("express-validator");
 
-const name = check("name", "Name is required.").not().isEmpty();
-const fileName = check("fileName", "Filename is required.").not().isEmpty(); 
+const fileName = check("fileName", "Filename is required.").not().isEmpty();
+const courses = check("courses", "Course is required.").not().isEmpty(); 
 
-const courseValidations = [ name, fileName ];
+const courseValidations = [ fileName, courses ];
+
 
 module.exports = courseValidations;
