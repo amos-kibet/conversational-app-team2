d467147b1bbfaf7b5a7670f8e0262fabad8e523d
amos-kibet <kibetamos97@gmail.com>
2022-12-14 11:04:54 +0300

    fix[refactor]: refactor code

---
+++ backend/crawler/crawl.js
@@ -0,0 +1,26 @@
+const request = require("request");
+
+const crawlerGET = (Url, cb) => {
+  request(
+    {
+      url: "https://schedge.a1liu.com/" + Url,
+      method: "GET",
+      json: true,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Methods": "POST,GET,OPTIONS",
+      },
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        cb(body);
+      } else {
+        console.log("Crawling Failed:" + "https://schedge.a1liu.com/" + Url);
+      }
+    }
+  );
+};
+
+module.exports = {
+  crawlerGET: crawlerGET,
+};
