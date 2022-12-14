b4b3650c1561d1c3ca81d2fd171180707fdeb21c
amos-kibet <kibetamos97@gmail.com>
2022-12-14 11:08:11 +0300

    fix: delete old code
    old code can be found on Develop branch

--- Backend/services/repo/course-validator.js
+++
@@ -1,8 +0,0 @@
-const { check } = require("express-validator");
-
-const fileName = check("fileName", "Filename is required.").not().isEmpty();
-const courses = check("courses", "Course is required.").not().isEmpty();
-
-const courseValidations = [fileName, courses];
-
-module.exports = courseValidations;
--- Backend/services/repo/validate.js
+++
@@ -1,9 +0,0 @@
-exports.username = async (model, payload) => {
-  const exists = model.findOne({ username: payload });
-  return exists;
-};
-
-exports.email = async (model, payload) => {
-  const exists = model.findOne({ email: payload });
-  return exists;
-};
